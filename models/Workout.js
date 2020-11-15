5f4f2d6c339df40b2dcf03e10bf621c718b31586
Laura Cole <lauracole1900@comcast.net>
2020-11-15 09:58:24 -0700

    workout model

---
+++ models/Workout.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: () => new Date()
+  },
+  exercises: [
+    {
+      type: {type: String, required: "Exercise type is required"},
+      name: {type: String, required: "Exercise name is required"},
+      duration: {type: Number, required: "Exercise duration is required"},
+      weight: {type: Number},
+      reps: {type: Number},
+      sets: {type: Number},
+      distance: {type: Number}
+    }]
+});
+
+WorkoutSchema.virtual("totalDuration").get(function() {
+  return this.exercises.reduce(function(total, exercise) {
+    return total + exercise.duration
+  }, 0)
+})
+
+const Workout = mongoose.model("Resistance", WorkoutSchema);
+
+module.exports = Workout;
